90f2f7d10b79758a3b0192309c9bf88f82fc3206
TinTin Choonchuachan <53388199+mistertfy69@users.noreply.github.com>
2019-11-05 19:42:34 +0700

    Updated files
    4 more classes of workers (S-Worker included)
    Changed multiplier "appearance" 
    Added a "Prestige" section that doesn't do anything at all.

--- userinterface.js
+++ userinterface.js
@@ -4,16 +4,26 @@ function interface_switch_workers(){
 	document.getElementById("interface_upgrades").style.display = "none";
 	document.getElementById("interface_workers").style.display = "block";
 	document.getElementById("interface_options").style.display = "none";
+	document.getElementById("interface_prestige").style.display = "none";
 };
 
 function interface_switch_upgrades(){
 	document.getElementById("interface_workers").style.display = "none";
 	document.getElementById("interface_upgrades").style.display = "block";
 	document.getElementById("interface_options").style.display = "none";
+	document.getElementById("interface_prestige").style.display = "none";
 };
 
 function interface_switch_options(){
 	document.getElementById("interface_workers").style.display = "none";
 	document.getElementById("interface_upgrades").style.display = "none";
 	document.getElementById("interface_options").style.display = "block";
+	document.getElementById("interface_prestige").style.display = "none";
+};
+
+function interface_switch_prestige(){
+	document.getElementById("interface_workers").style.display = "none";
+	document.getElementById("interface_upgrades").style.display = "none";
+	document.getElementById("interface_options").style.display = "none";
+	document.getElementById("interface_prestige").style.display = "block";
 };
